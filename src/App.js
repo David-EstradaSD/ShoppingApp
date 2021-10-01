cced829de8fb8577e7e78d072d47c98ace8ee1e2
David <destrada2193@gmail.com>
2021-10-01 15:17:21 -0700

    Connect to Firebase backend and handle side effects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-udemy-http-fd441-default-rtdb.firebaseio.com/cart.json", { 
+      method: "PUT", 
+      body: JSON.stringify(cart)
+     });
+  }, [cart]); // we will send this HttpRequest each time our Cart changes
 
   return (
     <Layout>
-      { showCart && <Cart /> }
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
