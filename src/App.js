aace2de5f4be34f15f446ba8637de4d25a79f559
David <destrada2193@gmail.com>
2021-10-03 13:19:47 -0700

    Complete sending data, need fetch logic still

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Products from "./components/Shop/Products";
 import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
 
+let isInitialState = true; // this is for the application start to prevent resetting the backend data with the async request
+
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
-  const notification = useSelector(state => state.ui.notification);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
     const sendCartData = async () => {
@@ -34,8 +36,12 @@ function App() {
          title: 'Success!',
          message: 'Sent cart data successfully.',
        }));
-
     };
+
+    if (isInitialState) {
+      isInitialState = false;
+      return; 
+    }
 
     sendCartData().catch((error) => {
       dispatch(uiActions.showNotification({
